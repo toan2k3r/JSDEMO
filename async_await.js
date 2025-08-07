b0994dea3784e95ce9226a746500796b25f63e97
Tên của bạn <toan2k3r@gmail.com>
2025-08-07 17:44:54 +0700

    a

--- async_await.js
+++ async_await.js
@@ -36,16 +36,16 @@ async function sendEmail(email) {
     })
 }
 async function sendAllEmails(emails) {
-    for(const email of emails){
+    for (const email of emails) {
         const result = await sendEmail(email);
         console.log(result);
     }
     console.log("Hoàn tất gửi email");
 }
-sendAllEmails(["user1@example.com","user2@example.com"])
+sendAllEmails(["user1@example.com", "user2@example.com"])
 //-----------------------------------------------------------------------
 async function unstableAPI() {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         const isSuccess = Math.random() > 0.5;
         setTimeout(() => {
             isSuccess ? resolve("Data loaded!") : reject("API failed")
@@ -56,43 +56,44 @@ async function fetchWithRetry(retries = 3) {
     try {
         const result = await unstableAPI();
         console.log("Thành công:", result);
-        
+
     } catch (error) {
         if (retries > 0) {
             console.log(`THử Lại...($(retries)lần còn lại)`);
-            await new Promise(resolve => setTimeout(resolve,1000));
-            return fetchWithRetry(retries -1);
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            return fetchWithRetry(retries - 1);
         }
-        console.error("Lỗi cuối cùng:",error);
-        
+        console.error("Lỗi cuối cùng:", error);
+
     }
 }
 fetchWithRetry()
 //----------------------------------------------------------------
 async function fetchSlowData() {
     return new Promise((resolve, reject) => {
-        const timeout = setTimeout(() => resolve("Data loaded!"),3000);
+        const timeout = setTimeout(() => resolve("Data loaded!"), 3000);
         const abort = () => {
             clearTimeout(timeout)
             reject(new Error("Request bị hủy!"))
         };
-        return {abort}
+        return { abort }
     });
 }
-
+const controller = new AbortController();
+const { signal } = controller
 async function run() {
-    const controller = new AbortController();
-    const {signal} = controller
+
     try {
-        const data = await new Promise(async(resolve,reject) => {
-            signal.addEventListener("abort",()=> reject(new Error("Hủy thủ công!")));
-            const result = await fetchSlowData();
+        const data = await new Promise(async (resolve, reject) => {
+            signal.addEventListener("abort", () => reject(new Error("Hủy thủ công!")));
+           
+            const {abort} = await fetchSlowData();
             resolve(result)
         });
         console.log(data);
-    } catch(error){
+    } catch (error) {
         console.error(error.message);
     }
 }
 run();
-setTimeout(() => controller.abort(),1500)
+setTimeout(() => controller.abort(), 1500)
